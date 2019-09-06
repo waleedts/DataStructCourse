8ec9f2763082525dbd795fa4d17ed5d245b8c239
Waleed Saleh <waleed_hp_2000@hotmail.com>
2019-09-06 21:44:29 +0300

    aesthetic modification

--- Linked List/Double/doubleLinkedList.h
+++ Linked List/Double/doubleLinkedList.h
@@ -1,18 +1,22 @@
 #ifndef _doubleLinkedList_h
 #define _doubleLinkedList_h
+
 class doubleLinkedList {
 private:
-    struct Node{
+    struct Node {
         int id;
         Node *next;
         Node *previous;
     };
-    Node *head= nullptr;
+    Node *head = nullptr;
 public:
     void insert(int);
+
     //void deleteN(int x);
     void deleteL();
+
     void displayB();
+
     void displayI();
 };
 
--- Linked List/Double/main.cpp
+++ Linked List/Double/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "doubleLinkedList.h"
+
 using namespace std;
 
 int main() {
